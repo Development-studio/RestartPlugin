f4c8e79ba41245a8c78ed60b10c80dff3bbd167d
mikhaillav <59438110+mikhaillav@users.noreply.github.com>
2021-09-08 17:07:03 +0300

    debug

--- restart.js
+++ restart.js
@@ -5,13 +5,13 @@ mc.regConsoleCmd("restart","restart the server",function(){
     setTimeout(() => { mc.broadcast("restart through 8 second"); }, 6000);
     setTimeout(() => { mc.broadcast("restart through 7 second"); }, 8000);
     setTimeout(() => { mc.broadcast("restart through 6 second"); }, 10000);
-    setTimeout(() => { mc.broadcast("restart through 5 second"); }, 14000);
-    setTimeout(() => { mc.broadcast("restart through 4 second"); }, 16000);
-    setTimeout(() => { mc.broadcast("restart through 3 second"); }, 18000);
-    setTimeout(() => { mc.broadcast("restart through 2 second"); }, 20000);
-    setTimeout(() => { mc.broadcast("restart through 1 second"); }, 22000);
-    setTimeout(() => { mc.broadcast("RESTART!"); }, 24000);
-    setTimeout(() => { mc.runcmdEx("stop") }, 26000);
+    setTimeout(() => { mc.broadcast("restart through 5 second"); }, 12000);
+    setTimeout(() => { mc.broadcast("restart through 4 second"); }, 14000);
+    setTimeout(() => { mc.broadcast("restart through 3 second"); }, 16000);
+    setTimeout(() => { mc.broadcast("restart through 2 second"); }, 18000);
+    setTimeout(() => { mc.broadcast("restart through 1 second"); }, 20000);
+    setTimeout(() => { mc.broadcast("RESTART!"); }, 21000);
+    setTimeout(() => { mc.runcmdEx("stop") }, 23000);
 
 })
 setInterval(function(){
